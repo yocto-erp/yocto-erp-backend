b1253502894b2a0730cb7b6c74bfa219b804308a
lephuoccanh <lephuoccanh@gmail.com>
2021-02-04 20:53:01 +0700

    Fix student fee update.

--- app/controller/web/tagging/tagging.controller.js
+++ app/controller/web/tagging/tagging.controller.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {hasPermission, isAuthenticated} from "../../middleware/permission";
+import {isAuthenticated} from "../../middleware/permission";
 import {pagingParse} from "../../middleware/paging.middleware";
 import {createTag, getListTags, readTagging, removeTagging} from "../../../service/tagging/tagging.service";
 
 const router = express.Router();
 
-router.get('/', [hasPermission(100), pagingParse({column: 'id', dir: 'asc'})], async (req, res, next) => {
+router.get('/', [isAuthenticated(), pagingParse({column: 'id', dir: 'asc'})], async (req, res, next) => {
   return getListTags(req.query, req.paging.order, req.paging.offset, req.paging.limit, req.user)
     .then(t => res.status(200).json(t))
     .catch(next);
